8bdfb6fa782770bccb4a1a369ca0390f290257ee
walterdfbr <walterdfbr@hotmail.com>
2020-07-21 14:13:14 -0300

    Departamento Classe Entidade

---
+++ src/application/Programa.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package application;
+
+import model.entities.Departamento;
+
+/**
+ *
+ * @author c051618
+ */
+public class Programa {
+    
+    public static void main(String[] args) {
+        Departamento obj = new Departamento(1, "Books");
+        System.out.println(obj);
+    }
+    
+}
